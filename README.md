8eca91604b052a640cadd1190ebd16aa4e4e1497
xtrkil <victorhuangwq@gmail.com>
2021-09-06 20:23:53 -0400

    Add toy example fib.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # github-recitation
-This repository is the base repository that students will fork and use for the git / github recitation
+This repository is the base repository that students will fork and use for the git / github recitation. 
+It contains a singular python file called fib.py, which is a toy example that students will modify, write pull requests as a pair.
---
+++ fib.py
@@ -0,0 +1,19 @@
+
+"""
+Fibonacci number generator
+When given a position, the function returns the fibonacci at that position in the sequence.
+The first number in the fibonacci sequence is 0.
+"""
+def fibonacci(position):
+  if(position == 2 or position == 3):
+    return 1
+  return fibonacci(position - 1) + fibonacci(position - 2)
+
+
+print("The 2nd Fibonacci number: ", fibonacci(2))
+print("The 21st Fibonacci number: ", fibonacci(21))
+
+print("The 1st Fibonacci number: ", fibonacci(1))
+print("The 0th Fibonacci number: ", fibonacci(0))
+
+

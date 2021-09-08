e524474dbe1ee501110b0e4b77d290e8e63e4d06
xtrkil <victorhuangwq@gmail.com>
2021-09-07 21:32:27 -0400

    Update fib.py

--- fib.py
+++ fib.py
@@ -2,19 +2,19 @@
 """
 Fibonacci number generator
 When given a position, the function returns the fibonacci at that position in the sequence.
-The first number in the fibonacci sequence is 0.
+The zeroth number in the fibonacci sequence is 0. The first number is 1
 """
 def fibonacci(position):
-  if(position == 2 or position == 3):
+  if(position == 1 or position == 2):
     return 1
   return fibonacci(position - 1) + fibonacci(position - 2)
 
 
 # Test cases
-print("The 2nd Fibonacci number: ", fibonacci(2))
-print("The 21st Fibonacci number: ", fibonacci(21))
 print("The 1st Fibonacci number: ", fibonacci(1))
-print("The 0th Fibonacci number: ", fibonacci(0))
+print("The 21st Fibonacci number: ", fibonacci(21))
+print("The 0th Fibonacci number: ", fibonacci(1))
+print("The -1st Fibonacci number: ", fibonacci(-1))
 
 print("Code ran successfully!")
 

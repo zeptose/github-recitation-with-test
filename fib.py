ae14baa3fc2cb298ed6b868425ae9620a850235e
zeptose <mcgrudy1232gmail.com>
2022-08-31 12:48:20 -0400

    fixed recursion bug

--- fib.py
+++ fib.py
@@ -6,7 +6,9 @@ The zeroth number in the fibonacci sequence is 0. The first number is 1
 Negative numbers should return None
 """
 def fibonacci(position):
-  if(position == 1 or position == 2):
+  if(position == 0):
+    return 0
+  elif (position == 1):
     return 1
   return fibonacci(position - 1) + fibonacci(position - 2)
 
